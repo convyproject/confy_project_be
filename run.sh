54f18bc3534c9edc7237136f11a3213a1a194441
Ramadhanu <ramadhanu@embreo.global>
2022-12-07 14:00:34 +0700

    uncomment build script in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 npm i
 npm i -g pm2
 pm2 del "Convy Backend"
-# npm run build --optimize_for_size --max_old_space_size=460 
+npm run build --optimize_for_size --max_old_space_size=460 
 pm2 start ./serve.sh --name "Convy Backend"
