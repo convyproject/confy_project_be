fd183ddd8924fb30a40a016522d7f5f8df2cb737
Ramadhanu <ramadhanu@embreo.global>
2022-12-07 14:15:13 +0700

    change max ram usage

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 npm i
 npm i -g pm2
 pm2 del "Convy Backend"
-npm run build --optimize_for_size --max_old_space_size=460 
+export NODE_OPTIONS=--max_old_space_size=4096
+npm run build
 pm2 start ./serve.sh --name "Convy Backend"
